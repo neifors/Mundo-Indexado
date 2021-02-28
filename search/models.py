ff61142e7a5c9b46a7c4f9136b19185c0b9a3eae
Isabel <isargp@gmail.com>
2021-02-28 10:18:31 +0100

    primer main commit (o no)

---
+++ search/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Article(models.Model):
+    description = models.TextField()
+    new_price = models.FloatField()
+    old_price = models.FloatField()
+    discount = models.FloatField()
+    img = models.ImageField()
+    href = models.URLField()
+
+class Web(models.Model):
+    name = models.CharField(max_length=100)
+    href = models.URLField(max_length=100)
+    articles = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    def to_Str(self):
+        return Web.__str__(self)
+
+    def create(name:str, href, articles):
+        return Web.objects.create(name=name, href=href, articles=articles)
+
+    def read():
+        return Web.objects.all()
+
+class Alert(models.Model):
+    email = models.EmailField()
+    price_limit = models.FloatField()
+    product = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+
