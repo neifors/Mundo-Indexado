2eb4d5d9da6aa5b4a43e4f35f7f28eb555d43409
Isabel Repetto <isargp@gmail.com>
2021-06-18 14:17:44 +0200

    dell added

--- search/scraping.py
+++ search/scraping.py
@@ -3,39 +3,42 @@ from bs4 import BeautifulSoup
 from requests import get
 import datetime, json
 import urllib.request
+from concurrent.futures import ThreadPoolExecutor
 
 
+
+#! RESPONSE [403] FORBIDDEN (Nos han bloqueado el acceso)
 # def pccomponentes(to_search):
     
-    # headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
-    # headers = ({'User-Agent':'Chrome/41.0.2228.0'})
-    # pc_componentes = 'https://www.pccomponentes.com' #pc_componentes+'/buscar/?query='+to_search+"&page="+str(page)+"&or-relevance"
-    # page = 1
-    # data=[]
-    # while page <= 4:
-    #     uri_to_search = f"https://www.pccomponentes.com/buscar/?query=auriculares&page={page}&or-relevance"   
-    #     print(uri_to_search)
-    #     response = get(uri_to_search, headers=headers)
-    #     print(response)
-    #     html_soup = BeautifulSoup(response.text, 'html.parser')
-    #     results_container = html_soup.find_all("a", class_="Wrapper-sc-8udqwp cIlLgX sc-amkrK dmBMsh")
+#     headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+#     headers = ({'User-Agent':'Chrome/41.0.2228.0'})
+#     pc_componentes = 'https://www.pccomponentes.com' #pc_componentes+'/buscar/?query='+to_search+"&page="+str(page)+"&or-relevance"
+#     page = 1
+#     data=[]
+#     while page <= 4:
+#         uri_to_search = f"https://www.pccomponentes.com/buscar/?query=auriculares&page={page}&or-relevance"   
+#         print(uri_to_search)
+#         response = get(uri_to_search, headers=headers)
+#         print(response)
+#         html_soup = BeautifulSoup(response.text, 'html.parser')
+#         results_container = html_soup.find_all("a", class_="Wrapper-sc-8udqwp cIlLgX sc-amkrK dmBMsh")
                                                         
-    #     if results_container != []:
-    #         for product in results_container:
-    #             current_price = product.find("span", class_ = "sc-jfkLlK bQYAGo" ) #precio actual si tiene descuento
-    #             if current_price != None:
-    #                 price = float(current_price.text.replace("€",""))
-    #                 description = product["data-name"]
-    #                 old_price = float(product["data-price"]) # precio antiguo si tiene aplicado descuento
-    #                 img = product.find('img')['src']
-    #                 href = product["href"]
-    #                 discount = (old_price-price)*100/old_price
-    #                 data.append({'description':description ,'price':price ,'discount':f"{discount:.2f}%" ,'old_price':old_price ,'img_url':img ,'product_href':href})
-    #     else:
-    #         print("sudo pollas de pccomponentes")
-    #     page += 1
+#         if results_container != []:
+#             for product in results_container:
+#                 current_price = product.find("span", class_ = "sc-jfkLlK bQYAGo" ) #precio actual si tiene descuento
+#                 if current_price != None:
+#                     price = float(current_price.text.replace("€",""))
+#                     description = product["data-name"]
+#                     old_price = float(product["data-price"]) # precio antiguo si tiene aplicado descuento
+#                     img = product.find('img')['src']
+#                     href = product["href"]
+#                     discount = (old_price-price)*100/old_price
+#                     data.append({'description':description ,'price':price ,'discount':f"{discount:.2f}%" ,'old_price':old_price ,'img_url':img ,'product_href':href})
+#         else:
+#             print("sudo pollas de pccomponentes")
+#         page += 1
         
-    # return data        
+#     return data        
 
 def mediamarkt (to_search):
     #class_ = ProductFlexBox__StyledListItem-nk9z2u-0 kzcilw
@@ -94,12 +97,9 @@ def backmarket(to_search):
                         continue
                    
                     current_price = float(product.find("span", class_="_3OcKBk8D _2SrrvPwuOVjCyULC_FKjin").text.replace("€","").replace(",","."))
-                    try:
-                        description = product.find("h2", class_="_2RGsPtNo JmqhBfpGehDVZ-r086xn8").text.strip()
-                    except AttributeError:
-                        description = product.find("h2", class_="_2RGsPtNo _2xkoCemRpVvAyafHpgIPdC").text.strip()                                      
+                    description = product.find("h2", class_="_2RGsPtNo").text.strip()
                     href = backmarket+product["href"]
-                    img = product.find("img")["src"]
+                    # img = product.find("img")["src"]
                     discount = (old_price-current_price)*100/old_price
                     data.append({'description':description ,'price':current_price ,'discount':f"{discount:.2f}%" ,'old_price':old_price,'product_href':href})
                     
@@ -108,24 +108,57 @@ def backmarket(to_search):
 
     return data
           
+          
+def dell(to_search):
+    
+    headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+    dell = 'https://www.dell.com'
+    page = 1
+    data=[]
+    while page <= 4:
+        
+        uri_to_search = dell+"/es-es/search/"+to_search+"?p="+str(page)+"&t=Product"
+        response = get(uri_to_search , headers=headers)
+        ##? Si response 200:
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        results_container = html_soup.find_all("article", class_ = 'stack-accessories ps-stack')
+    
+        if results_container != []:
+            for product in results_container:
+                check_price = product.find("div", class_="ps-orig ps-simplified" )
+                if check_price:
+                    old_price = float(check_price.find("span", class_="strike-through").text.replace(".","").replace(",",".").replace("€",""))
+                    current_price = float(product.find("div", class_="ps-dell-price ps-simplified").find_all("span")[1].text.replace(".","").replace(",",".").replace("€",""))
+                    description = product.find("h3", class_="ps-title").text.strip()
+                    href = product.find("h3", class_="ps-title").find("a")["href"][2:]
+                    discount = (old_price-current_price)*100/old_price
+                    data.append({'description':description ,'price':current_price ,'discount':f"{discount:.2f}%" ,'old_price':old_price,'product_href':href})
+                else:
+                    continue
+        page += 1
+
+    return data
+    
+    
+def tien21(to_search):
+    pass
     
     
 def super_search_to_json(to_search):
     
     list_result = [{"date":f"{datetime.datetime.now()}"}]
-    # data_pccomp = pccomponentes(to_search)
-    data_mediamarkt = mediamarkt(to_search)
-    data_backm = backmarket(to_search)
-    list_result.extend(data_backm)
-    list_result.extend(data_mediamarkt)
+
+    with ThreadPoolExecutor() as executor:
+        data_dell = executor.submit(dell, to_search=to_search)
+        data_mediamarkt = executor.submit(mediamarkt, to_search=to_search)
+        data_backm = executor.submit(backmarket, to_search=to_search)
+        
+    list_result.extend(data_backm.result())
+    list_result.extend(data_mediamarkt.result())
+    list_result.extend(data_dell.result())
         
     # with open(f"./search/backup/{to_search}.json", "w", encoding= "utf8") as file:
     #     json.dump( list_result, file, ensure_ascii = False)
     
-    
-
     return list_result
     
-# super_search_to_json("teclado")
-# print(pccomponentes("teclado"))
-# mediamarkt("raton")
